6dc85966370c55b573e21fd2d3ca08d32393b71d
omarelkeshen <oelkeshen5@gmail.com>
2024-11-26 17:06:21 +0200

    create footer

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import styles from './Footer.module.css';
+const { footerContainer } = styles;
+
+const Footer = () => {
+  return <div className={footerContainer}>&copy; 2025 Our Ecom. All rights reserved.</div>;
+};
+
+export default Footer;
