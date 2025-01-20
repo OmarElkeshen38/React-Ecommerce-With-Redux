60d64b5790338a8d2b11ab6423140ac8f5f13d70
OmarElkeshen38 <oelkeshen5@gmail.com>
2025-01-20 17:07:42 +0200

    fix categories image rendaring

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -10,12 +10,14 @@ const Categories = () => {
   const { records, loading, error } = useAppSelector((state) => state.categories);
 
   useEffect(() => {
-    dispatch(actGetCategories());
+    if (!records.length) {
+      dispatch(actGetCategories());
+    }
   }, [dispatch]);
 
   const categoriesList = records.length > 0 ? records.map((record) => (
 
-    <Col  key={record.id} xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
+    <Col key={record.id} xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
       <Category {...record} />
     </Col>
   )) : "There are no categories";
